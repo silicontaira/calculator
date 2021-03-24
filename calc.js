6e7a7d4909099019a776a54939c256718cc065f6
firebat2003 <spencermtaira@gmail.com>
2021-03-23 18:24:27 -0700

    Allowed calculator to perform multiple operations in a row. Implemented decimal, percent change, and sign change buttons. Still working on adding keyboard functionality and will do final testing after.

--- calc.js
+++ calc.js
@@ -8,35 +8,73 @@ let operator;
 let newNumFlag = 0;                             //status flag created to know when to append numbers together or replace them on the screen
                                                 //i.e. pressing 3 twice in a row should equal 33 but without the flag it would just end up replacing 3 with 3
 let finalNum = 0;
+let intNum = 0;                                 //temp number for multiple operation presses
+let decFlag = 0;                                //prevents user from using multiple decimals in one number
 const numButts = document.querySelectorAll('.numButt');
 const opButts = document.querySelectorAll('.opButt');
 const eqlButt = document.querySelector('#equal');
 const clrButt = document.querySelector('#clear');
 const screen = document.querySelector('#screen');
+const decimal = document.querySelector('#dot');
+const percent = document.querySelector('#percent');
+const signChange = document.querySelector('#signChange');
 
+//Event listener for anytime a number button is pressed
 numButts.forEach(numButt => numButt.addEventListener('click', function() { 
     let tempNum = parseFloat(this.textContent);
     display(tempNum);
     displayNum = parseFloat(screen.textContent);
 }));
+
+//Event listener for anytime an operation button is pressed
 opButts.forEach(opButt => opButt.addEventListener('click', opPress));
-eqlButt.addEventListener('click', function() {
+
+//Event listener for "=" button press
+eqlButt.addEventListener('click', equals);
+
+//Event listener for "AC" button press
+clrButt.addEventListener('click', clear);
+
+//Event listener for "." button press
+decimal.addEventListener('click', function() {
+    if (decFlag == 0) {
+        console.log(this.textContent);
+        let dec = this.textContent;
+        display(dec);
+        displayNum = parseFloat(screen.textContent);
+        decFlag = 1;    
+    }
+});
+
+percent.addEventListener('click', function() {
+    tempNum = parseFloat(screen.textContent)/100;
+    newNumFlag = 1;
+    display(tempNum);
+    newNumFlag = 1;
+});
+
+signChange.addEventListener('click', function() {
+    tempNum = parseFloat(screen.textContent) * -1;
+    newNumFlag = 1;
+    display(tempNum);
+    newNumFlag = 1;
+});
+
+//Functions
+function equals(e) {
     if (operator == undefined) {
         return;
     }
     else {
         console.log(firstNum, displayNum);
         finalNum = operate(operator, firstNum, displayNum);
-        firstNum = finalNum;
         console.log(finalNum);
+        firstNum = finalNum;
         newNumFlag = 1;
         display(finalNum);
         newNumFlag = 1;
     }
-});
-clrButt.addEventListener('click', clear);
-
-//Functions
+}
 function operate(operator, a, b) {
     if (operator == 'sum') {
         return sum(a, b);
@@ -69,17 +107,33 @@ function display(num) {                             //Updates calc screen
 
 function clear(e) {
     screen.textContent = 0;
-    firstnum = 0;
+    firstNum = 0;
     displayNum = 0;
     total = 0;
     operator = undefined;
     newNumFlag = 0;
+    decFlag = 0;
 }
 
 function opPress(e) {
-    firstNum = displayNum;
+    prevOperator = operator;
     operator = this.id;
+    if(firstNum == 0) {
+        firstNum = displayNum;
+    }
+    else{
+        if (prevOperator == undefined) {
+            intNum = operate(operator, firstNum, displayNum);
+        }
+        else {
+            intNum = operate(prevOperator, firstNum, displayNum);
+        }
+        newNumFlag = 1;
+        display(intNum);
+        firstNum = intNum;
+    }
     newNumFlag = 1;
+    decFlag = 0;
 }
 
 //Basic math functions
@@ -96,12 +150,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if (b == 0) return "You can't divide by 0 silly!";
+    if (b == 0) return "ERROR";
     return a / b;
 }
 
 /******************errors
-problem: after '=' is pushed when new number is entered it appends to the displayText
-solution: new number should overwrite displayText
+ Add keyboard functionality
                          ****************/
                         
